a83d56c7c2656d059e89df40ee6af67ee90c5ce7
John <jbrilhar@gmail.com>
2020-11-03 22:58:43 -0500

    Adding Python version of ACID Demo with Postgre

---
+++ Postgre(ACID DEMO).py
@@ -0,0 +1,38 @@
+import psycopg2
+
+#connect to the db
+con = psycopg2.connect(
+            host = "localhost",
+            database="postgres",
+            user = "postgres",
+            password = "Pfunk4life!")
+#cursor`
+#For isolation: SERIALIZABLE
+con.set_isolation_level(3)
+#For atomicity
+con.autocommit = False
+try:
+    cur = con.cursor()
+    #Either the next two statements are executed are not at all
+    cur.execute("insert into employees (id, name) values (%s, %s)", (4, "Jeff"))
+    cur.execute("insert into employees (id, name) values (%s, %s)", (5, "Suzy"))
+    #execute query
+    cur.execute("select id, name from employees")
+    rows = cur.fetchall()
+    for r in rows:
+        print(f"id {r[0]} name {r[1]}")
+
+except Exception as err:
+    print(err)
+    print("Transactions could not be completed so database will be rolled back before start of transactions")
+    con.rollback()
+
+
+#commit the transcation
+con.commit()
+
+#close the cursor
+cur.close()
+
+#close the connection
+con.close()
